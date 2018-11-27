bf235ab9c4abcba022d5c001e897648ed24ee7d7
krzysztofstefanski <krzysztof.stefanski82@gmail.com>
2018-11-27 16:30:07 +0100

    add nextStep , prevStep , handlech

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -10,6 +10,26 @@ state = {
     city:'',
     bio:''
 }
+//Proced to next step
+nextStep = () => {
+    const {step} = this.state
+    this.setState({step : step + 1
+})
+}
+//Proced to previous step
+
+prevStep = () => {
+    const {step} = this.state
+    this.setState({step : step - 1
+})
+}
+
+//Handle fields change
+handleChange = input => e => {
+    this.setState({[input]: e.target})
+}
+
+}
 
   render() {
     return (
