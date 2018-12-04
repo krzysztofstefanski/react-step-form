ef2b76c883a3ff2cc40b087d3d31b302011ab171
Krzysztof Stefanski <39494955+krzysztofstefanski@users.noreply.github.com>
2018-12-04 23:06:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Reactt
           </a>
         </header>
       </div>
